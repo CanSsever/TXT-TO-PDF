5f1f743a505000b1c9c2e72f515ef7436f46b24b
Can Sever <cansevver01@gmail.com>
2025-06-17 13:14:28 +0200

    add each text from .txt files to pdf file

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ for filepath in filepaths:
 
     filename = Path(filepath).stem
     animal_name = filename.split('.')[0]
+
     name = animal_name.capitalize()
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=name)
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+
+    pdf.set_font(family="Times", size=16)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
+
 
 pdf.output("output.pdf")
